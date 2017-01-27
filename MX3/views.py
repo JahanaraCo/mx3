5ecefc5e2bb99033920d3d6f517f17e59a7b7ee4
Mohammad Mahdi Jahanara <mmjahanara@ce.sharif.edu>
2017-01-27 22:19:08 +0000

    final visual changes, signup issue solved.

--- MX3/views.py
+++ MX3/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
+from datetime import datetime
 
 
 def publishers_list(request):
@@ -20,15 +21,15 @@ def publisher_info(request, pid):
 def game_info(request, gtid):
     gametitle = Gametitlet.objects.all().get(gtid=gtid)
 
-	if request.method == "POST":
-		gamer = Gamert.objects.all().get(username= request.user.username)
-		if gamer.user_account_balance < gametitle.price:
+    if request.method == "POST":
+        gamer = Gamert.objects.all().get(username= request.user.username)
+        if gamer.user_account_balance < gametitle.price:
             return HttpResponse('your balance is not enough! <br/> <a href="/game/%s"> back to game page </a>'%gametitle.gtid)
-		gamer.user_account_balance -= gametitle.price
-		gamer.save()
-		buy = Buyt(gid=gamer.gid, gtid=gametitle.gtid, date=datetime.now())
-		buy.save()
-		return HttpResponse('you bought this game!  <br/> <a href="/game/%s"> back to game page </a>'%gametitle.gtid)
+        gamer.user_account_balance -= gametitle.price
+        gamer.save()
+        buy = Buyt(gid=gamer, gtid=gametitle, date=datetime.now())
+        buy.save()
+        return HttpResponse('you bought this game!  <br/> <a href="/game/%s"> back to game page </a>'%gametitle.gtid)
 
     own_this_game = False
     if request.user.is_authenticated:
@@ -61,15 +62,15 @@ def gamer_signup(request):
         check_unique2 = Gamert.objects.all().filter(username = username)
         check_unique3 = Gamert.objects.all().filter(username = username)
 
-        if check_unique1.count() > 0 or check_unique2.count() > 0 or check_unique3.count() > 0:
+        if check_unique1.count() > 0 or check_unique2.count() > 0 or check_unique3.count() > 0 or len(password) < 8:
             return HttpResponse('Invalid! <br/> <a href="/login"> try again </a>')
 
-        django_user = User(username = username, password = password)
+        django_user = User(username=username, password=password, email=email)
+        django_user.set_password(password)
         django_user.save()
-        user_account_balance = 0
         gamer_gid = Gamert.objects.all().count() + 1
-        gamer = Gamert(gamer_gid, username = username, password = password, email = email,
-                phone_number = phone_number, django_user = django_user, user_account_balance = 0)
+        gamer = Gamert(gid=gamer_gid, name=name, username=username, password=password, email=email,
+                phone_number=phone_number, django_user=django_user, user_account_balance=0)
         gamer.save()
         login(request, django_user)
         return HttpResponseRedirect('/gamer/%s'%(django_user.username))
